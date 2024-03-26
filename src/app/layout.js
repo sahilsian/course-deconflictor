9d2ef73f42c2fe35b8dc273fa8cf915070d7b80a
sahilsian <sahilsian101@gmail.com>
2024-03-26 16:49:57 -0300

    suspense added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactQueryClientProvider } from "../../components/ReactQueryClientProvider";
-
+import { Suspense } from 'react'
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -12,11 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <Suspense>
     <ReactQueryClientProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
     </ReactQueryClientProvider>
+    </Suspense>
 
   );
 }
